39474be97f97fcc6abd7c63f71524362b984cdc3
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-09-19 18:20:16 -0500

    solution to error with installation eslint

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit/dist';
 import reducerBooks from './books/books';
 import reducerCategories from './categories/categories';
 
